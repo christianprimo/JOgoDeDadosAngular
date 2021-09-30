d50bfa5b4291cbf2efe0d2be88fa297b582ac347
christianprimo <66392231+christianprimo@users.noreply.github.com>
2021-09-30 14:41:17 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,2 +1,16 @@
-angular.module("meuModulo", ['componentes']);
+'use strict';
+
+// Declare app level module which depends on views, and core components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version',
+  'componentes'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
 angular.module("componentes", []);
